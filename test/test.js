cc8a6e59a4350795b0873337da78760451b004c8
harsh0301 <shah.03harsh@gmail.com>
2022-10-06 15:07:40 -0400

    final changes

--- test/test.js
+++ test/test.js
@@ -10,10 +10,19 @@ describe("SAMPLE unit test",function(){
     // #1 should return home page
   
     it("Checking healthz api",function(done){
+  
+      // calling home page api
+      check
+      .get("/healthz")
       
-      console.log("Hello world")
-      done();
-      
+      .expect(200) // THis is HTTP response
+      .end(function(err,res){
+        // HTTP status should be 200
+        res.status.should.equal(200);
+        // Error key should be false.
+        res.body.error.should.equal(false);
+        done();
+      });
     });
   
   });
