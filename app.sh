7291384c0c48eeed56e62899f282f95e1bd024d7
harsh0301 <shah.03harsh@gmail.com>
2022-11-09 12:55:11 -0500

    check logs updated

--- app.sh
+++ app.sh
@@ -18,7 +18,7 @@ sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
     -a fetch-config \
     -m ec2 \
-    -c file:/home/ec2-user/webapp/cloudwatch-config.json \
+    -c file:/home/ubuntu/webapp/cloudwatch-config.json \
     -s
 
 sudo apt-get install -y nodejs
