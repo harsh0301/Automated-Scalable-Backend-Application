95a8647607e490df8587fe8999d7b577e5983b44
Harsh Shah <98559913+harsh0301@users.noreply.github.com>
2022-11-15 21:22:20 -0500

    A8 01 (#60)
    * check log cloudwatch
    
    * check log cloudwatch
    
    * updated app.sh
    
    * logs
    
    * added winston
    
    * checkk logs
    
    * check logs
    
    * check logs
    
    * check logs
    
    * check logs updated
    
    * check logs
    
    * added statsd
    
    * check logs and statsd
    
    * added logs
    
    * added statsd time
    
    * added for email verification

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,18 +3,30 @@ const User = db.users;
 // image Upload
 const multer = require('multer')
 const path = require('path')
-const { uuid } = require('uuidv4');
+const {
+  v4: uuidv4
+} = require('uuid');
 const bcrypt = require('bcrypt');
 const moment= require('moment');
 const emailValidator = require("email-validator");
 const winston = require('winston');
 const logger=require("../config/logger")
 const SDC = require('statsd-client')
-
-
 const sdc = new SDC({host:"localhost", port:8125});
+const AWS = require('aws-sdk');
+AWS.config.update({
+    region: process.env.AWS_REGION || 'us-east-1'
+});
+var sns = new AWS.SNS({});
+var dynamoDatabase = new AWS.DynamoDB({
+    apiVersion: '2012-08-10',
+    region: process.env.AWS_REGION || 'us-east-1'
+});
+
+
 // create main Model
 const Product = db.products
+
 
 
 // main work
@@ -24,55 +36,110 @@ const Product = db.products
 const addProduct = async (req, res) => {
   logger.info('Add a user');
   sdc.increment("endpoint.Add_user");
-    if (
-      !req.body.username ||
-      !req.body.first_name ||
-      !req.body.last_name ||
-      !req.body.password
-    ) {
-      res.status(400).send();
-    } else {
-      // generate salt to hash password
-      const salt = await bcrypt.genSalt(10);
-      const hashPassword = await bcrypt.hash(req.body.password, salt);
-  
-      let info = {
-        id:uuid(),
-        username: req.body.username,
-        last_name: req.body.last_name,
-        first_name: req.body.first_name,
-        password: hashPassword,
-      };
-  
-      if (
-        !emailValidator.validate(`${req.body.username}`) ||
-        !req.body.first_name ||
-        !req.body.last_name
-      ) {
-        res.status(400).send();
-      } else {
-        const findUser = await User.findOne({
-          where: { username: `${req.body.username}` },
-        });
-        if (findUser === null) {
-          const user = await User.create(info).then((data) => {
-            let plainUser = {
-              id: data.id,
-              username: data.username,
-              first_name: data.first_name,
-              last_name: data.last_name,
-              account_created: data.createdAt,
-              account_updated: data.updatedAt,
-            };
-  
-            res.status(201).json(plainUser);
-          });
-  
-          res.status(201).send();
-        } else {
-          res.status(400).send();
-        }
+  var hash = await bcrypt.hash(req.body.password, 10);
+  const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+  if (!emailRegex.test(req.body.username)) {
+      logger.info("/create user 400");
+      res.status(400).send({
+          message: 'Enter your Email ID in correct format. Example: abc@xyz.com'
+      });
+  }
+  const getUser = await User.findOne({
+      where: {
+          username: req.body.username
       }
+  }).catch(err => {
+      logger.error("/create user error 500");
+      res.status(500).send({
+          message: err.message || 'Some error occurred while creating the user'
+      });
+  });
+
+  console.log('verified and existing 1');
+
+ 
+  if (getUser) {
+      console.log('verified and existing', getUser.dataValues.isVerified);
+      var msg = getUser.dataValues.isVerified ? 'User already exists! & verified' : 'User already exists! & not verified';
+      console.log('verified and existing msg' ,msg);
+      
+      res.status(400).send({
+          message: msg
+      });
+  } else {
+      var user = {
+          id: uuidv4(),
+          first_name: req.body.first_name,
+          last_name: req.body.last_name,
+          password: hash,
+          username: req.body.username,
+          isVerified: false
+      };
+      console.log('above user');
+      User.create(user).then(async udata => {
+
+              const randomnanoID = uuidv4();
+
+              const expiryTime = new Date().getTime();
+
+              // Create the Service interface for dynamoDB
+              var parameter = {
+                  TableName: 'csye-6225',
+                  Item: {
+                      'Email': {
+                          S: udata.username
+                      },
+                      'TokenName': {
+                          S: randomnanoID
+                      },
+                      'TimeToLive': {
+                          N: expiryTime.toString()
+                      }
+                  }
+              };
+              console.log('after user');
+              //saving the token onto the dynamo DB
+              try {
+                  var dydb = await dynamoDatabase.putItem(parameter).promise();
+                  console.log('try dynamoDatabase', dydb);
+              } catch (err) {
+                  console.log('err dynamoDatabase', err);
+              }
+
+              console.log('dynamoDatabase', dydb);
+              var msg = {
+                  'username': udata.username,
+                  'token': randomnanoID
+              };
+              console.log(JSON.stringify(msg));
+
+              const params = {
+
+                  Message: JSON.stringify(msg),
+                  Subject: randomnanoID,
+                  TopicArn: 'arn:aws:sns:us-east-1:680057889162:verify_email'
+
+              }
+              var publishTextPromise = await sns.publish(params).promise();
+
+              console.log('publishTextPromise', publishTextPromise);
+              res.status(201).send({
+                  id: udata.id,
+                  first_name: udata.first_name,
+                  last_name: udata.last_name,
+                  username: udata.username,
+                  account_created: udata.createdAt,
+                  account_updated: udata.updatedAt,
+                  isVerified: udata.isVerified
+              });
+
+          })
+          .catch(err => {
+              logger.error(" Error while creating the user! 500");
+              res.status(500).send({
+                  message: err.message || "Some error occurred while creating the user!"
+              });
+          });
     }
   };
 
